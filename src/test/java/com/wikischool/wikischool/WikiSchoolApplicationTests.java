019f36ab8391471ef5a2e5b5ab6da1d732609715
Sean Harnett <sean.harnett97@gmail.com>
2021-03-15 15:52:32 +0100

    javadoc, began sql implementation, and created more tests

--- src/test/java/com/wikischool/wikischool/WikiSchoolApplicationTests.java
+++ src/test/java/com/wikischool/wikischool/WikiSchoolApplicationTests.java
@@ -14,8 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
--- src/test/java/com/wikischool/wikischool/WikiTestConstants.java
+++ src/test/java/com/wikischool/wikischool/WikiTestConstants.java
@@ -3,7 +3,11 @@ package com.wikischool.wikischool;
 
 /**
  * This abstract class is used to store constants that can be used in Application tests.
+ * @author sean-harnett
  */
 public abstract class WikiTestConstants {
+    /**
+     * Location of the properties file to test database connectivity.
+     */
     static final public String TEST_DATABASE_CONNECTION_PROPERTIES_FILE_LOCATION =  "src/main/java/com/wikischool/wikischool/databaseConnectionPropertiesFile.properties";
 }
